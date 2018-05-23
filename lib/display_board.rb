a5de32146c37603e4e48e72698a6385baf69ff86
Lamido Tijjani <kinglamido@gmail.com>
2018-05-23 13:09:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,3 @@ def display_board(board)
   ttt = " #{board[0]} | #{board[1]} | #{board[2]} \n#{n}\n #{board[3]} | #{board[4]} | #{board[5]} \n#{n}\n #{board[6]} | #{board[7]} | #{board[8]} "
   puts ttt
 end
-
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
